14053ef8e3c6c1e727118c519e86e79e6003514d
installer <installer@gmail.com>
2026-01-10 16:22:57 +0700

    add vpn client

--- master/modules/home/dev-tools.nix
+++ master/modules/home/dev-tools.nix
@@ -1,23 +1,19 @@
 {pkgs, ...}: {
   # Development tools configuration
   home.packages = with pkgs; [
-    # Laravel-specific tools
-    laravel-pail # Laravel log viewer
-    
     # Node.js development tools
     nodePackages.prettier # Code formatter
     nodePackages.eslint # JavaScript linter
     nodePackages.typescript # TypeScript support
-    
+
     # Python development tools
-    black # Python formatter
-    pylint # Python linter
-    mypy # Python static type checker
-    pytest # Python testing framework
-    
+    python3Packages.black # Python formatter
+    python3Packages.pylint # Python linter
+    python3Packages.mypy # Python static type checker
+    python3Packages.pytest # Python testing framework
+
     # General development
     git-lfs # Git Large File Storage
-    just # Command runner
   ];
 
   # Home directory variables for development
---
+++ master/modules/vpn.nix
@@ -0,0 +1,48 @@
+{pkgs, ...}: {
+  # VPN support with GUI client
+  
+  # Enable NetworkManager for VPN GUI support
+  networking.networkmanager = {
+    enable = true;
+    # VPN plugins
+    plugins = with pkgs; [
+      networkmanager-openvpn # OpenVPN support
+      networkmanager-l2tp # L2TP/IPSec support
+    ];
+  };
+
+  # VPN client packages
+  environment.systemPackages = with pkgs; [
+    # OpenVPN
+    openvpn # OpenVPN client
+
+    # PPTP VPN
+    pptpclient # PPTP client
+    pptp # PPTP protocol support
+
+    # L2TP/IPSec
+    xl2tpd # L2TP daemon
+    libreswan # IPSec support (required for L2TP)
+
+    # GUI VPN Manager
+    networkmanagerapplet # NetworkManager GUI applet
+
+    # VPN utilities
+    wireguard-tools # WireGuard support (bonus)
+  ];
+
+  # Enable necessary kernel modules for VPN
+  boot.kernelModules = [
+    "ppp_generic"
+    "ppp_async"
+    "pptp"
+    "l2tp_core"
+    "l2tp_ppp"
+    "xfrm4_tunnel"
+  ];
+
+  # VPN-related environment variables
+  environment.variables = {
+    OPENVPN_DIR = "/etc/openvpn";
+  };
+}
